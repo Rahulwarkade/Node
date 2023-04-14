f967d634b5210f19fe225fbb366ab67f0c38c13d
Arule <88571832+Rahulwarkade@users.noreply.github.com>
2023-04-14 17:32:00 +0530

    Sasta vs Code

--- routes/index.js
+++ routes/index.js
@@ -3,9 +3,8 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res) {
-  res.render('index',{name : req.query.name});
-  
-});
+  res.render('index');
+  });
 
 router.post('/about', function(req, res) {
   res.render('about');
@@ -14,7 +13,8 @@ router.post('/about', function(req, res) {
 router.get('/contact', function(req, res) {
   res.render('contact');
 });
-// router.get('/output', function(req, res) {
-//   // console.log(req.query.name);
-// });
+router.get('/output', function(req, res) {
+  res.render('output',{name:req.query.username});
+  console.log(req.query);
+});
 module.exports = router;
